86567ef83f395becc6916266b9881313c6a20f3a
Michail Kaika <mishakaika@gmail.com>
2023-02-28 21:13:39 +0100

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,20 @@
+let random1 = Math.floor(Math.random() * 6) + 1;
+let random2 = Math.floor(Math.random() * 6) + 1;
+let dice1 = document.querySelector(".img1");
+let dice2 = document.querySelector(".img2");
+// let dice1 = document.getElementsByClassName("img1");
+
+randomSrc1 = "images/dice" + random1 + ".png";
+randomSrc2 = "images/dice" + random2 + ".png";
+dice1.setAttribute("src", randomSrc1);
+console.log("fff");
+dice2.setAttribute("src", randomSrc2);
+
+let h1 = document.querySelector(".container h1");
+if (random1 > random2){
+    h1.innerHTML = '😎 Player 1 Wins!';
+} else if (random2 > random1){
+    h1.textContent = "Player 2 Wins! 😎";
+} else {
+    h1.textContent = "Draw!";
+}
